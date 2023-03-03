44d424eb9591547499438dada784426c422471ad
EBliujus <ev.bliujus@gmail.com>
2023-03-03 14:37:49 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.scss';
+import axios from 'axios';
 
 function App() {
+
+
+    const [jokes, setJokes] = useState(null);
+
+
+    useEffect(() => {
+      axios.get('https://v2.jokeapi.dev/joke/Programming?amount=10')
+      .then(res => {
+        // console.log(res.data);
+        setJokes(res.data)
+      })
+
+    }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1>Jokes</h1>
+        <ul>
+          {
+            jokes.map(j => <li key={j.id}>{}</li> )
+          }
+        </ul>
       </header>
     </div>
   );
