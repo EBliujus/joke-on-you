f04d9427f38a5d455b5c0e0327f607be6d9e575c
EBliujus <ev.bliujus@gmail.com>
2023-03-04 22:47:20 +0200

    fixed to show jokes, still not perfect

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import axios from 'axios';
 function App() {
 
 
-    const [joke, setJoke] = useState(null);
+    const [joke, setJoke] = useState([]);
 
 
     useEffect(() => {
       axios.get('https://v2.jokeapi.dev/joke/Programming?amount=10')
       .then(res => {
         console.log(res.data);
-        setJoke(res.data.map((j, i) => ({...j, row: i})));
+        setJoke(res.data.jokes.map((j, i) => ({...j, row: i})));
       })
 
     }, []);
@@ -24,7 +24,11 @@ function App() {
         <h1>Programmer Jokes</h1>
         <ul>
           {
-           joke ? joke.map(j => <li key={j.id}>{j.jokes}</li> ) : <li>LOADING...</li>
+           joke
+          ? 
+          joke.map(j => <p key={j.type === 'joke'}>{j.joke}</p>)
+          :
+           <p>{joke.error}</p>
           }
         </ul>
       </header>
